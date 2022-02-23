0bcc3f9269f01ae0fb1308085fd5a852bec8643c
Julien Xavier <jxavier@ippon.fr>
2022-02-23 23:42:25 +0100

    refactoring: refactoring code & add JSdoc

--- typescript/src/Yatzy.ts
+++ typescript/src/Yatzy.ts
@@ -1,197 +1,197 @@
 export default class Yatzy {
-  private dice: number[];
+  
+  private dices: number[];
 
-  constructor(d1: number, d2: number, d3: number, d4: number, _5: number) {
-    this.dice = [];
-    this.dice[0] = d1;
-    this.dice[1] = d2;
-    this.dice[2] = d3;
-    this.dice[3] = d4;
-    this.dice[4] = _5;
+  constructor(...args : number[]) {
+    this.dices = args;
   }
 
-  static chance(d1: number, d2: number, d3: number, d4: number, d5: number): number {
-    var total = 0;
-    total += d1;
-    total += d2;
-    total += d3;
-    total += d4;
-    total += d5;
-    return total;
+  /**
+   * Static method to sum dices that correspond to n.
+   * @param n  number to find
+   * @param dices list of dices
+   * @returns the sum of matching dices.
+   */
+  static sumByValues(n : number, dices : number[]): number {
+    return dices.filter(v => v == n).reduce((p, c) => p + c, 0);
   }
 
-  static yatzy(...args: number[]): number {
-    var counts = [0, 0, 0, 0, 0, 0, 0, 0];
-    for (var i = 0; i != args.length; ++i) {
-      var die = args[i];
-      counts[die - 1]++;
-    }
-    for (i = 0; i != 6; i++) if (counts[i] == 5) return 50;
-    return 0;
+  /**
+   * Private method to sum dices that correspond to n (in a class environnement).
+   * @param n  number to find
+   * @param dices list of dices
+   * @returns the sum of matching dices.
+   */
+  private sumByValue(n : number, dices : number[]): number {
+    return dices.filter(v => v == n).reduce((p, c) => p + c, 0);
   }
 
-  static ones(d1: number, d2: number, d3: number, d4: number, d5: number): number {
-    var sum = 0;
-    if (d1 == 1) sum++;
-    if (d2 == 1) sum++;
-    if (d3 == 1) sum++;
-    if (d4 == 1) sum++;
-    if (d5 == 1) sum++;
-
-    return sum;
+  /**
+   * The player scores the sum of the dice that reads one.
+   * @param dices list of dices
+   * @returns the sum of matching dices.
+   */
+  static ones(...dices: number[]): number {
+    return this.sumByValues(1, dices);
   }
 
-  static twos(d1: number, d2: number, d3: number, d4: number, d5: number): number {
-    var sum = 0;
-    if (d1 == 2) sum += 2;
-    if (d2 == 2) sum += 2;
-    if (d3 == 2) sum += 2;
-    if (d4 == 2) sum += 2;
-    if (d5 == 2) sum += 2;
-    return sum;
+  /**
+   * The player scores the sum of the dice that reads two.
+   * @param dices list of dices
+   * @returns the sum of matching dices.
+   */
+  static twos(...dices: number[]): number {
+    return this.sumByValues(2, dices);
   }
 
-  static threes(d1: number, d2: number, d3: number, d4: number, d5: number): number {
-    var s;
-    s = 0;
-    if (d1 == 3) s += 3;
-    if (d2 == 3) s += 3;
-    if (d3 == 3) s += 3;
-    if (d4 == 3) s += 3;
-    if (d5 == 3) s += 3;
-    return s;
+  /**
+   * The player scores the sum of the dice that reads three.
+   * @param dices list of dices
+   * @returns the sum of matching dices.
+   */
+  static threes(...dices: number[]): number {
+    return this.sumByValues(3, dices);
   }
 
-  static score_pair(d1: number, d2: number, d3: number, d4: number, d5: number): number {
-    var counts = [0, 0, 0, 0, 0, 0, 0, 0, 0];
-    counts[d1 - 1]++;
-    counts[d2 - 1]++;
-    counts[d3 - 1]++;
-    counts[d4 - 1]++;
-    counts[d5 - 1]++;
-    var at;
-    for (at = 0; at != 6; at++) if (counts[6 - at - 1] >= 2) return (6 - at) * 2;
-    return 0;
-  }
-
-  static two_pair(d1: number, d2: number, d3: number, d4: number, d5: number): number {
-    var counts = [0, 0, 0, 0, 0, 0, 0, 0, 0];
-    counts[d1 - 1]++;
-    counts[d2 - 1]++;
-    counts[d3 - 1]++;
-    counts[d4 - 1]++;
-    counts[d5 - 1]++;
-    var n = 0;
-    var score = 0;
-    for (let i = 0; i < 6; i += 1)
-      if (counts[6 - i - 1] >= 2) {
-        n++;
-        score += 6 - i;
-      }
-    if (n == 2) return score * 2;
-    else return 0;
-  }
-
-  static four_of_a_kind(_1: number, _2: number, d3: number, d4: number, d5: number): number {
-    var tallies;
-    tallies = [0, 0, 0, 0, 0, 0, 0, 0];
-    tallies[_1 - 1]++;
-    tallies[_2 - 1]++;
-    tallies[d3 - 1]++;
-    tallies[d4 - 1]++;
-    tallies[d5 - 1]++;
-    for (let i = 0; i < 6; i++) if (tallies[i] >= 4) return (i + 1) * 4;
-    return 0;
-  }
-
-  static three_of_a_kind(d1: number, d2: number, d3: number, d4: number, d5: number): number {
-    var t;
-    t = [0, 0, 0, 0, 0, 0, 0, 0, 0];
-    t[d1 - 1]++;
-    t[d2 - 1]++;
-    t[d3 - 1]++;
-    t[d4 - 1]++;
-    t[d5 - 1]++;
-    for (let i = 0; i < 6; i++) if (t[i] >= 3) return (i + 1) * 3;
-    return 0;
-  }
-
-  static smallStraight(d1: number, d2: number, d3: number, d4: number, d5: number): number {
-    var tallies;
-    tallies = [0, 0, 0, 0, 0, 0, 0];
-    tallies[d1 - 1] += 1;
-    tallies[d2 - 1] += 1;
-    tallies[d3 - 1] += 1;
-    tallies[d4 - 1] += 1;
-    tallies[d5 - 1] += 1;
-    if (tallies[0] == 1 && tallies[1] == 1 && tallies[2] == 1 && tallies[3] == 1 && tallies[4] == 1) return 15;
-    return 0;
-  }
-
-  static largeStraight(d1: number, d2: number, d3: number, d4: number, d5: number): number {
-    var tallies;
-    tallies = [0, 0, 0, 0, 0, 0, 0, 0];
-    tallies[d1 - 1] += 1;
-    tallies[d2 - 1] += 1;
-    tallies[d3 - 1] += 1;
-    tallies[d4 - 1] += 1;
-    tallies[d5 - 1] += 1;
-    if (tallies[1] == 1 && tallies[2] == 1 && tallies[3] == 1 && tallies[4] == 1 && tallies[5] == 1) return 20;
-    return 0;
-  }
-
-  static fullHouse(d1: number, d2: number, d3: number, d4: number, d5: number): number {
-    var tallies;
-    var _2 = false;
-    var i;
-    var _2_at = 0;
-    var _3 = false;
-    var _3_at = 0;
-
-    tallies = [0, 0, 0, 0, 0, 0, 0, 0];
-    tallies[d1 - 1] += 1;
-    tallies[d2 - 1] += 1;
-    tallies[d3 - 1] += 1;
-    tallies[d4 - 1] += 1;
-    tallies[d5 - 1] += 1;
-
-    for (i = 0; i != 6; i += 1)
-      if (tallies[i] == 2) {
-        _2 = true;
-        _2_at = i + 1;
-      }
-
-    for (i = 0; i != 6; i += 1)
-      if (tallies[i] == 3) {
-        _3 = true;
-        _3_at = i + 1;
-      }
-
-    if (_2 && _3) return _2_at * 2 + _3_at * 3;
-    else return 0;
-  }
-
+  /**
+   * The player scores the sum of the dice that reads four.
+   * @param dices list of dices
+   * @returns the sum of matching dices.
+   */
   fours(): number {
-    var sum;
-    sum = 0;
-    for (let at = 0; at != 5; at++) {
-      if (this.dice[at] == 4) {
-        sum += 4;
+    return this.sumByValue(4, this.dices);
+  }
+
+  /**
+   * The player scores the sum of the dice that reads five.
+   * @param dices list of dices
+   * @returns the sum of matching dices.
+   */
+  fives(): number {
+    return this.sumByValue(5, this.dices);
+  }
+
+  /**
+   * The player scores the sum of the dice that reads six.
+   * @param dices list of dices
+   * @returns the sum of matching dices.
+   */
+  sixes(): number {
+    return this.sumByValue(6, this.dices);
+  }
+
+  /**
+   * The player scores the sum of all dice, no matter what they read.
+   * @param dices dices
+   * @returns the sum of matching dices.
+   */
+  static chance(...dices: number[]): number {
+    return dices.reduce((p, c) => p + c);
+  }
+
+  /**
+   * If all dice have the same number, the player scores 50 points.
+   * @param dices dices
+   * @returns the sum of matching dices.
+   */
+  static yatzy(...dices: number[]): number {
+    return dices.every(v => v === dices[0]) ? 50 : 0;
+  }
+
+  /**
+   * The player scores the sum of the two highest matching dice.
+   * @param dices dices
+   * @returns the sum of matching dices.
+   */
+  static score_pair(...dices: number[]): number {
+    var sorted_array = dices.sort((a,b) => b - a);
+    for(var i = 0 ; i < sorted_array.length - 1 ; i++){
+      if(sorted_array[i] === sorted_array[i + 1]){
+        return sorted_array[i] * 2;
       }
     }
+    return 0;
+  }
+
+  /**
+   * If there are two pairs of dice with the same number, the player scores the sum of these dice.
+   * @param dices dices
+   * @returns the sum of matching dices.
+   */
+  static two_pair(...dices: number[]): number {
+    var sum = 0;
+    sum += this.score_pair(...dices);
+    var filtered_array = dices.filter(v => v !== sum / 2);
+    sum += this.score_pair(...filtered_array);
     return sum;
   }
-
-  fives(): number {
-    let s = 0;
-    var i;
-    for (i = 0; i < this.dice.length; i++) if (this.dice[i] == 5) s = s + 5;
-    return s;
+  
+  /**
+   * If there are three dice with the same number, the player scores the sum of these dice.
+   * @param dices dices
+   * @returns the sum of matching dices.
+   */
+  static three_of_a_kind(...dices: number[]): number {
+    var sorted_array = dices.sort((a,b) => b - a);
+    for(var i = 0 ; i < sorted_array.length - 2 ; i++){
+      if((sorted_array[i] == sorted_array[i + 1]) &&
+         (sorted_array[i + 1] == sorted_array[i + 2])){
+        return sorted_array[i] + sorted_array[i + 1] + sorted_array[i + 2];
+      }
+    }
+    return 0;
   }
 
-  sixes(): number {
-    let sum = 0;
-    for (var at = 0; at < this.dice.length; at++) if (this.dice[at] == 6) sum = sum + 6;
+  /**
+   * If there are four dice with the same number, the player scores the sum of these dice.
+   * @param dices dices
+   * @returns the sum of matching dices.
+   */
+  static four_of_a_kind(...dices: number[]): number {
+    var sorted_array = dices.sort((a,b) => b - a);
+    for(var i = 0 ; i < sorted_array.length - 3 ; i++){
+      if((sorted_array[i] == sorted_array[i + 1]) &&
+         (sorted_array[i + 1] == sorted_array[i + 2]) &&
+         (sorted_array[i + 2] == sorted_array[i + 3])){
+        return sorted_array[i] + sorted_array[i + 1] 
+             + sorted_array[i + 2] + sorted_array[i + 3];
+      }
+    }
+    return 0;
+  }
+
+  /**
+   * If the dice read "1,2,3,4,5" the player scores the sum of the dices.
+   * @param dices dices
+   * @returns the sum of matching dices.
+   */
+  static smallStraight(...dices: number[]): number {
+    var sorted_array = dices.sort((a,b) => a - b);
+    return sorted_array[0] == 1 && sorted_array[1] == 2 
+    && sorted_array[2] == 3 && sorted_array[3] == 4 && sorted_array[4] == 5 ? 15 : 0;
+  }
+
+  /**
+   * If the dice read "2,3,4,5,6" the player scores the sum of the dices.
+   * @param dices dices
+   * @returns the sum of matching dices.
+   */
+  static largeStraight(...dices: number[]): number {
+    var sorted_array = dices.sort((a,b) => a - b);
+    return sorted_array[0] == 2 && sorted_array[1] == 3 
+    && sorted_array[2] == 4 && sorted_array[3] == 5 && sorted_array[4] == 6 ? 20 : 0;
+  }
+
+  /**
+   * If the dice are two of a kind and three of a kind, the player scores the sum of all the dice.
+   * @param dices dices
+   * @returns the sum of matching dices.
+   */
+  static fullHouse(...dices: number[]): number {
+    var sum = 0;
+    sum += this.score_pair(...dices);
+    var filtered_array = dices.filter(v => v !== sum / 2);
+    sum += this.three_of_a_kind(...filtered_array);
     return sum;
   }
 }
